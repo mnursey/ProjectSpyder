ffc0ab3186637ef637dc0a153ba68495afbbebb6
mnursey <mitchnursey@gmail.com>
2020-11-08 03:09:43 -0800

    Merge

---
+++ Assets/Scripts/DebugCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCamera : MonoBehaviour
+{
+    public Transform focus;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = focus.transform.position + offset;
+    }
+}
