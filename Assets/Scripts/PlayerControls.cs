debfe18deff60b866ec1e993a20dbd99dab1a855
Ian <ianrdejong@gmail.com>
2020-11-07 16:02:49 -0800

    Begin adding some player controls

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask selectableObjectsLayer = LayerMask.GetMask("Everything");
+
+    private List<GameObject> selectedObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedObjects = new List<GameObject>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitResult;
+            Debug.Log("Casting ray");
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitResult, selectableObjects))
+            {
+                Debug.Log("hit something");
+                selectedObjects.Add(hitResult.collider.gameObject);
+                Debug.Log(hitResult.collider.gameObject);
+            }
+        }
+    }
+}
