ac1996d65bbbd38607e0f0a972c62c6624405ace
Ian <ianrdejong@gmail.com>
2020-11-10 15:09:09 -0800

    Making vehicles explode

---
+++ Assets/Scripts/AutoDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyer : MonoBehaviour
+{
+    public float lifespan = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.Destroy(gameObject, lifespan);
+    }
+}
